9174211e979b3e57a30f8ac02b158e1908ca4dd5
kodythach <kodythach@gmail.com>
2020-04-27 21:34:55 -0400

    Base game completed

--- rooms/GameRoom.ts
+++ rooms/GameRoom.ts
@@ -1,28 +1,73 @@
 import { Room } from "colyseus";
 
 export class GameRoom extends Room {
-  playerList: any = [];
+  playerList: any = {};
+  currentCardsLeft: Array<any> = [];
+  undefinedIndexes: Array<Number> = [];
+
   onCreate (options: any) {
     console.log("Game room created!", options);
 
+    this.populateCardList();
+
     this.onMessage("message", (client, message) => {
-      console.log("ChatRoom received message from", client.sessionId, ":", message);
-      this.broadcast("messages", `${client.sessionId}: ${message}`);
+      this.broadcast("messages", `[${this.playerList[client.sessionId]}]: ${message}`);
+    });
+
+    this.onMessage("name", (client, message) => {
+      this.playerList[client.sessionId] = message;
+      this.broadcast("userJoined", `${ message } joined.`);
+      this.broadcast("updatePlayerList",  this.getPlayerNameList());
+      client.send("updateCardsLeft", this.undefinedIndexes);
+    });
+
+    this.onMessage("getCardResult", (client, message) => {
+      this.broadcast("displayCardResult", this.currentCardsLeft[parseInt(message)]);
+      this.broadcast("drawMessage", `${this.playerList[client.sessionId]} drew a \
+        ${this.currentCardsLeft[parseInt(message)].value} of ${this.currentCardsLeft[parseInt(message)].suit}`);
+      this.undefinedIndexes.push(parseInt(message));
+      this.broadcast("updateCardsLeft", this.undefinedIndexes);
     });
   }
 
   onJoin(client: any) {
-    this.playerList.push(client.sessionId);
-    this.broadcast("messages", `${ client.sessionId } joined.`);
+    this.playerList[client.sessionId] = '';
   }
 
   onLeave(client: any) {
-      this.playerList = this.playerList.filter((p: string) => p != client.sessionId);
-      this.broadcast("messages", `${ client.sessionId } left.`);
+      if (this.playerList[client.sessionId])
+        this.broadcast("userLeft", `${ this.playerList[client.sessionId] } left.`);
+      delete this.playerList[client.sessionId];
+      this.broadcast("updatePlayerList", this.getPlayerNameList());
   }
 
   onDispose () {
       console.log("Dispose GameRoom");
   }
 
+  populateCardList () {
+    const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+    const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+    for (let suit in suits) {
+      for (let value in values) {
+        this.currentCardsLeft.push({"suit": `${suits[suit]}`, "value": `${values[value]}`});
+      }
+    }
+
+    for (let i = this.currentCardsLeft.length-1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = this.currentCardsLeft[i];
+      this.currentCardsLeft[i] = this.currentCardsLeft[j];
+      this.currentCardsLeft[j] = temp;
+    }
+  }
+
+  getPlayerNameList () {
+    let playerNameList = [];
+    for (let id in this.playerList) {
+      playerNameList.push(this.playerList[id]);
+    }
+    return playerNameList;
+  }
 }
